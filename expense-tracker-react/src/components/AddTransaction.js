80633534c9447ffdc28416e0dd1ce2532d358c81
PratikaNG <pratika.05ng@gmail.com>
2023-03-21 14:30:42 +0530

    final commit

--- expense-tracker-react/src/components/AddTransaction.js
+++ expense-tracker-react/src/components/AddTransaction.js
@@ -1,16 +1,30 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
 
 const AddTransaction = () => {
   const [text, setText] = useState("");
   const [amount, setAmount] = useState(0);
 
+  const { addTransaction } = useContext(GlobalContext);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    const newTransaction = {
+      id: Math.floor(Math.random() * 100000000),
+      text,
+      amount: +amount,
+    };
+
+    addTransaction(newTransaction);
+  };
+
   return (
-    <form id="form">
+    <form onSubmit={onSubmit}>
       <div className="form-control">
         <label htmlFor="text">Text</label>
         <input
           type="text"
-          id="text"
           value={text}
           onChange={(e) => setText(e.target.value)}
           placeholder="Enter text..."
@@ -22,9 +36,8 @@ const AddTransaction = () => {
         </label>
         <input
           type="number"
-          id="amount"
           value={amount}
-          onChange={(e) => setAmount(e.target.amount)}
+          onChange={(e) => setAmount(e.target.value)}
           placeholder="Enter amount..."
         ></input>
       </div>
--- expense-tracker-react/src/components/Balance.js
+++ expense-tracker-react/src/components/Balance.js
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
+// import { useContext } from "react";
 
 const Balance = () => {
+  const { transactions } = useContext(GlobalContext);
+
+  const amounts = transactions.map((transaction) => transaction.amount);
+  console.log(amounts);
+  const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
+
   return (
     <div>
       <h4>Your Balance</h4>
-      <h1 id="balance">$0.50</h1>
+      <h1>${total}</h1>
     </div>
   );
 };
